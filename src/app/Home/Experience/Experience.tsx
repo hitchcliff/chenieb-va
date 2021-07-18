ca60e80eb9d1fb4e8755ac383b861761af541a5b
kevin 700brands <kevin@700brands.com>
2021-07-18 14:55:07 +0800

    update

--- src/app/Home/Experience/Experience.tsx
+++ src/app/Home/Experience/Experience.tsx
@@ -1,3 +1,25 @@
+import { Container } from '../../../components';
+import Heading, { HeadingPattern } from '../../../components/Heading/Heading';
+import { experienceData } from './ExperienceData';
+
 export default function Experience() {
-  return <div>experience</div>;
+  const experienceItems = experienceData.map(({ title, accomplishments }, idx) => (
+    <div key={idx}>
+      <h4>{title}</h4>
+    </div>
+  ));
+
+  return (
+    <Container className="py-24">
+      <div className="w-full">
+        <Heading
+          heading={HeadingPattern.H2}
+          className="text-center flex justify-center items-center w-full"
+        >
+          work experience
+        </Heading>
+        <div>{experienceItems}</div>
+      </div>
+    </Container>
+  );
 }
---
+++ src/app/Home/Experience/ExperienceData.ts
@@ -0,0 +1,27 @@
+export const experienceData = [
+  {
+    title: 'Service Crew in Tita Funnies',
+    accomplishments: [
+      `As a service crew my duty is  to serve our customers in a very nice way, like always saying “Good morning”, “Good afternoon”, “Thank you for coming”.`,
+      `We always make sure that the food we serve is clean. Moreover, we maintained fresh, and the right amount of food we serve, because we value customers.`,
+      `I have initiative during work, if one of my co-workers needs my help, I'm always there to help not only to impress everybody but because I love helping people. Even though I'm a service crew I'm also a dishwasher and kitchen assistant and waitress.`,
+      `Lastly is being punctual. It is very necessary if you have work, coming early to attend your duty is very basic but the most important is to complete your working hours, willing to adjust, and ready to work for extra hours specially occasions like valentine's day.`,
+    ],
+  },
+  {
+    title: 'Customer Service in Delicious Bakeshop',
+    accomplishments: [
+      `We need to wear proper hygiene to prevent accidents and to show customers that we have higher standards in terms of cleanliness.`,
+      `We have to make sure that we give our best service to each and every of our customers.`,
+      `I always smile at the customer, respect them, and treat them equally open to any learning.`,
+    ],
+  },
+  {
+    title: 'Shampoo Assistant in T3 Salon',
+    accomplishments: [
+      `To enhance my skill in doing Foot SPA manicure, and pedicure, I go through various training about Hair Color, Rebound, and Hairstylist in this establishment. It was fun and I learned a lot with their teachings.`,
+      `I was diligent enough to be offered a long-term position not just as a student but as a working partner.`,
+      `I learned how to socialize with people, smile and have confidence. Their guidance kept me on track.`,
+    ],
+  },
+];
